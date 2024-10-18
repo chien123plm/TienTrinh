c222abe6132d9e68d2a257caf4f213a1813532fc
Anh <Hoanganhnguyengia@hotmail.com>
2024-10-18 13:31:51 +0700

    them giao dien profile user

---
+++ QlyNhaHang/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QlyNhaHang.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult QuanLi()
+        {
+            return View();
+        }
+
+        public ActionResult QuanLiDatBan() { return View(); }
+        public ActionResult QuanLiKho() { return View(); }
+        public ActionResult QuanLiNhanVien() { return View(); }
+        public ActionResult QuanLiCaLam() { return View(); }
+    }
+}
